071a19b3d91dbd1648abfcb00ef2b8ae978da5e3
wisecavc <137959931+wisecavc@users.noreply.github.com>
2025-01-19 18:17:14 -0800

    Update sample-code.cpp
    changed name in window title

--- sample-code.cpp
+++ sample-code.cpp
@@ -51,7 +51,7 @@
 
 // title of these windows:
 
-const char *WINDOWTITLE = "OpenGL / GLUT Sample -- Joe Graphics";
+const char *WINDOWTITLE = "OpenGL / GLUT Sample -- Cade Wisecaver";
 const char *GLUITITLE   = "User Interface Window";
 
 // what the glui package defines as true and false:
